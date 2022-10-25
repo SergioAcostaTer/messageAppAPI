941ee4c5432300eb9a9e32ab70f94bafba33ff1d
Sergio Acosta <sergioacostaquintana@gmail.com>
2022-10-25 16:23:58 +0100

    make it better

---
+++ routes/getFriends.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const friendListSchema = require("../models/friendListSchema");
+
+const router = express.Router();
+
+router.post("/friends/get/:username", async (req, res) => {
+  const { username } = req.params;
+  let matchFriendList = await friendListSchema
+    .find({ user: username })
+    .select("-_id -__v");
+
+  if (matchFriendList[0]) {
+    res.json(matchFriendList[0])
+  }
+  if (!matchFriendList[0]) {
+    res.json({status: "Doesn't exist"})
+  }
+});
+
+module.exports = router;
